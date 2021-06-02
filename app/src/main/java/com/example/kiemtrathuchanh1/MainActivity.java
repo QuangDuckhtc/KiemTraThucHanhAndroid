e37ddce4993c420936b0c9e9d3f88a5f62dae6b2
QuangDuckhtc <nguyenquangduc.khtc@gmail.com>
2021-06-02 08:41:46 +0700

    hoan thien nút thoát + login firebase

--- app/src/main/java/com/example/kiemtrathuchanh1/MainActivity.java
+++ app/src/main/java/com/example/kiemtrathuchanh1/MainActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,7 +19,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     EditText pn_Email, pn_password;
-    Button btn_Login;
+    Button btn_Login, btn_exit;
+    TextView tv_Exit;
     private FirebaseAuth mAuth;
 
     @Override
@@ -29,14 +31,24 @@ public class MainActivity extends AppCompatActivity {
         pn_Email = findViewById(R.id.pl_email);
         pn_password = findViewById(R.id.pl_password);
         mAuth = FirebaseAuth.getInstance();
+       btn_exit = findViewById(R.id.btn_exit);
         btn_Login = findViewById(R.id.btn_Submit);
+
+        btn_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+        // chức năng
+
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = pn_Email.getText().toString();
                 String password = pn_password.getText().toString();
                 DangNhap(email,password);
-                if(TextUtils.isEmpty(email) |TextUtils.isEmpty(password)){
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
                     Toast.makeText(MainActivity.this, "Ko duoc rong", Toast.LENGTH_SHORT).show();
                 }
                 else{
@@ -55,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                         if(task.isSuccessful()){
                             //Toast.makeText(MainActivity.this, "Sucess", Toast.LENGTH_SHORT).show();
                             Intent intent = new Intent(MainActivity.this, Manager.class);
+                            startActivity(intent);
                         }
                         else{
                             Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
